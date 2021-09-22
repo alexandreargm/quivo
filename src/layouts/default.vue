2165d3436e3dc236e8973540e66c555ea9dae30c
alexandreargm <alexandreargm@gmail.com>
2021-09-23 00:33:23 +0200

    Created defalt layout

---
+++ src/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="default-layout">
+    <the-header />
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/TheHeader'
+
+export default {
+  name: 'defaultLayout',
+  components: { TheHeader }
+}
+</script>
