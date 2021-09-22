b6d7178101fac79a3c30c6f1bc00febafa955218
alexandreargm <alexandreargm@gmail.com>
2021-09-23 00:33:06 +0200

    Storybook now loading scss styles and css custom properties properly

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -6,5 +8,19 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'postcss-loader', 
+        {
+          loader: 'sass-loader',
+          options: {
+            prependData: '@import "../assets/scss/abstracts/_all.scss"; @import "../assets/scss/main.scss";'
+          }
+        }
+      ],
+    });
+    return config;
+  },
 }
