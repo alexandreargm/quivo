04f3816bc51982b60594280bc4d34afabdcae698
alexandreargm <alexandreargm@gmail.com>
2021-09-28 23:48:07 +0200

    Created theHeader component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="the-header">
+    <the-main-nav />
+  </header>
+</template>
+
+<script>
+import TheMainNav from '@/components/TheMainNav'
+
+export default {
+  components: { TheMainNav }
+}
+</script>
+
+<style lang='scss' scoped>
+.the-header {
+
+}
+</style>
