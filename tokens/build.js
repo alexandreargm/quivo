351c7882bda2201308a68516aa183c9cdc983b02
alexandreargm <alexandreargm@gmail.com>
2021-11-04 21:29:55 +0100

    Scss maps are now named accordingly to json file

--- tokens/build.js
+++ tokens/build.js
@@ -2,14 +2,17 @@ const StyleDictionaryPackage = require('style-dictionary')
 
 // HAVE THE STYLE DICTIONARY CONFIG DYNAMICALLY GENERATED
 
-// StyleDictionaryPackage.registerFormat({
-//   name: 'css/variables',
-//   formatter: function (dictionary, config) {
-//     return `${this.selector} {
-//         ${dictionary.allProperties.map(prop => `  --${prop.name}: ${prop.value};`).join('\n')}
-//       }`
-//   }
-// })
+StyleDictionaryPackage.registerFormat({
+  name: 'css/variables',
+  formatter: function (dictionary, config) {
+    return `$${this.selector}: (
+      ${dictionary.allProperties.map(prop => ` '${prop.name}': ${prop.value},`).join('\n')}
+    )`
+    // return `${this.selector} {
+    //     ${dictionary.allProperties.map(prop => `  --${prop.name}: ${prop.value};`).join('\n')}
+    //   }`
+  }
+})
 
 StyleDictionaryPackage.registerTransform({
   name: 'sizes/px',
@@ -30,59 +33,18 @@ function getStyleDictionaryConfig (theme) {
       `tokens/${theme}.json`
     ],
     platforms: {
-      web: {
+      'scss-map': {
         transforms: ['attribute/cti', 'name/cti/kebab', 'sizes/px'],
         buildPath: 'src/assets/scss/abstracts/tokens/',
         files: [{
           destination: `${theme}.scss`,
-          format: 'scss/map-flat'
-          // selector: `.${theme}-theme`
+          format: 'scss/map-flat',
+          selector: theme
         }]
       }
     }
   }
 }
-
-// StyleDictionaryPackage.registerFormat({
-//   name: 'css/variables',
-//   formatter: function (dictionary, config) {
-//     return `${this.selector} {
-//         ${dictionary.allProperties.map(prop => `  --${prop.name}: ${prop.value};`).join('\n')}
-//       }`
-//   }
-// })
-
-// StyleDictionaryPackage.registerTransform({
-//   name: 'sizes/px',
-//   type: 'value',
-//   matcher: function (prop) {
-//     // You can be more specific here if you only want 'em' units for font sizes
-//     return ['fontSizes', 'spacing', 'borderRadius', 'borderWidth', 'sizing'].includes(prop.attributes.category)
-//   },
-//   transformer: function (prop) {
-//     // You can also modify the value here if you want to convert pixels to ems
-//     return parseFloat(prop.original.value) + 'px'
-//   }
-// })
-
-// function getStyleDictionaryConfig (theme) {
-//   return {
-//     source: [
-//       `tokens/${theme}.json`
-//     ],
-//     platforms: {
-//       web: {
-//         transforms: ['attribute/cti', 'name/cti/kebab', 'sizes/px'],
-//         buildPath: 'output/',
-//         files: [{
-//           destination: `${theme}.css`,
-//           format: 'css/variables',
-//           selector: `.${theme}-theme`
-//         }]
-//       }
-//     }
-//   }
-// }
 
 console.log('Build started...');
 
@@ -94,7 +56,7 @@ console.log('Build started...');
 
   const StyleDictionary = StyleDictionaryPackage.extend(getStyleDictionaryConfig(theme))
 
-  StyleDictionary.buildPlatform('web')
+  StyleDictionary.buildPlatform('scss-map')
 
   console.log('\nEnd processing')
 })
