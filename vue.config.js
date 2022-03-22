b9530ca47a4e3aa125c3af28c2a6554305f7f5f3
alexandreargm <alexandreargm@gmail.com>
2022-03-22 21:38:04 +0100

    build: add package bundle analizer and added deploy script

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
 module.exports = {
   // Set route for gh-pages. Ref.: https://cli.vuejs.org/config/#publicpath
-  publicPath: process.env.NODE_ENV === 'production' ? '/quivo/' : '/',
+  // publicPath: process.env.NODE_ENV === 'production' ? '/quivo/' : '/',
   css: {
     // ref: https://cli.vuejs.org/guide/css.html#passing-options-to-pre-processor-loaders
     loaderOptions: {
@@ -8,5 +10,11 @@ module.exports = {
         prependData: '@import "@/assets/scss/abstracts/_all.scss";'
       }
     }
+  },
+  // https://cli.vuejs.org/guide/webpack.html#simple-configuration
+  configureWebpack: {
+    plugins: [
+      // new BundleAnalyzerPlugin()
+    ]
   }
 }
