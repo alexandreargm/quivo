d60f017a0db1c7018172787d68a75c5fd8a51c0b
alexandreargm <alexandreargm@gmail.com>
2022-03-25 16:44:58 +0100

    deploy

---
+++ precache-manifest.d6b96187f8a5c58dd3ed112918d8a43f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e24b2f83fcffc9fe66d",
+    "url": "/quivo/css/app.8ba57c8d.css"
+  },
+  {
+    "revision": "b5bf407061f9be0d5294",
+    "url": "/quivo/css/chunk-1ff40802.362b9aea.css"
+  },
+  {
+    "revision": "3ed9647d6a885805c8f9",
+    "url": "/quivo/css/chunk-391cbac0.ed66bc6f.css"
+  },
+  {
+    "revision": "62de2197421a28029521",
+    "url": "/quivo/css/chunk-abbff5da.41a62add.css"
+  },
+  {
+    "revision": "43fddeab410293736c84",
+    "url": "/quivo/css/chunk-e42a1918.e354cf93.css"
+  },
+  {
+    "revision": "e0bd8df3333ba167e136",
+    "url": "/quivo/css/chunk-ea445bca.37f8aea5.css"
+  },
+  {
+    "revision": "a93bae82f63feb277861",
+    "url": "/quivo/css/chunk-ff858710.4cbd55c4.css"
+  },
+  {
+    "revision": "064f1a9a490205edefd9b8a418c91cb5",
+    "url": "/quivo/index.html"
+  },
+  {
+    "revision": "1e24b2f83fcffc9fe66d",
+    "url": "/quivo/js/app.15d3d244.js"
+  },
+  {
+    "revision": "b5bf407061f9be0d5294",
+    "url": "/quivo/js/chunk-1ff40802.4d2652ff.js"
+  },
+  {
+    "revision": "3ed9647d6a885805c8f9",
+    "url": "/quivo/js/chunk-391cbac0.9ea87149.js"
+  },
+  {
+    "revision": "62de2197421a28029521",
+    "url": "/quivo/js/chunk-abbff5da.20283b91.js"
+  },
+  {
+    "revision": "43fddeab410293736c84",
+    "url": "/quivo/js/chunk-e42a1918.a56874e9.js"
+  },
+  {
+    "revision": "e0bd8df3333ba167e136",
+    "url": "/quivo/js/chunk-ea445bca.6be3b3e8.js"
+  },
+  {
+    "revision": "a93bae82f63feb277861",
+    "url": "/quivo/js/chunk-ff858710.114e07e6.js"
+  },
+  {
+    "revision": "fbda8f654f71f0390196",
+    "url": "/quivo/js/chunk-vendors.2b656b46.js"
+  },
+  {
+    "revision": "535256a008ccd6ac6b66411f05f5a9c1",
+    "url": "/quivo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/quivo/robots.txt"
+  }
+]);
