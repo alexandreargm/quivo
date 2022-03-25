744587bb2de8dff926ca8b0aa604f7b774f44404
alexandreargm <alexandreargm@gmail.com>
2022-03-25 15:14:22 +0100

    fix: set correct repository branch

--- gh-deploy.sh
+++ gh-deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:alexandreargm/quivo.git main:gh-pages
+git push -f git@github.com:alexandreargm/quivo.git master:gh-pages
 
 cd -
