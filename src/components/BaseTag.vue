20a2ab40f3d75fe5cbd2e666fbb086287f28efd9
alexandreargm <alexandreargm@gmail.com>
2022-10-08 12:16:47 +0200

    feat(components): add title and color props

--- src/components/BaseTag.vue
+++ src/components/BaseTag.vue
@@ -3,7 +3,9 @@
     class="base-tag"
     :class="[color]"
   >
-    <slot />
+    <slot>
+      {{ title }}
+    </slot>
   </span>
 </template>
 
@@ -12,33 +14,44 @@ export default {
   props: {
     color: {
       type: String,
-      default: 'base',
+      default: '',
       validator: function (value) {
-        return ['base'].includes(value)
+        return ['', 'interactive', 'danger'].includes(value)
       }
+    },
+    title: {
+      type: String,
+      default: ''
     }
-  },
-
-  setup () {
-
   }
 }
 </script>
 
 <style lang='scss' scoped>
 .base-tag {
+  --bg-color: var(--background-secondary);
+  --color: var(--text-neutral);
+
   align-items: center;
+  background-color: var(--bg-color);
   border-radius: var(--rounded40);
+  color: var(--color);
   display: flex;
+  font-size: var(--font-20);
   height: var(--size10);
   justify-content: center;
   padding: 0 var(--space-20);
+  user-select: none;
 }
 
 // Color
-.base-tag.base {
-  background-color: var(--background-secondary);
-  color: var(--text-neutral);
-  font-size: var(--font-20);
+.base-tag.interactive {
+  --bg-color: var(--icon-interactive);
+  --color: var(--text-neutral);
+}
+
+.base-tag.danger {
+  --bg-color: var(--icon-danger);
+  --color: var(--text-neutral);
 }
 </style>
