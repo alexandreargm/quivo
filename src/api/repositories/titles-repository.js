cc1793aef62d766329df6f92908ad4cdf5b98ebb
alexandreargm <alexandreargm@gmail.com>
2022-10-08 15:23:04 +0200

    feat(repositories): add search repository function

--- src/api/repositories/titles-repository.js
+++ src/api/repositories/titles-repository.js
@@ -1,4 +1,5 @@
 import client from '../axios-client'
+import { getIncludedExcludedValues, encodeValues, getDateRangeValues } from '@/composables/useTagFilter'
 
 const validMediaTypes = ['movie', 'tv']
 
@@ -13,6 +14,33 @@ export default {
     checkIsValidMediaType(mediaType)
 
     return client.get(`/${mediaType}/${id}?append_to_response=release_dates`)
+  },
+
+  search ({ mediaType, title = '', keywords = [], genres = [], dateRange = ['', ''] }) {
+    checkIsValidMediaType(mediaType)
+    const { includes: includedKeywords, excludes: excludedKeywords } = getIncludedExcludedValues(keywords)
+    const { includes: includedGenres, excludes: excludedGenres } = getIncludedExcludedValues(genres)
+    const [startDate, endDate] = getDateRangeValues(dateRange)
+
+    const queryParameters = [
+      ['with_text_query=', title],
+      ['with_keywords=', encodeValues(includedKeywords, 'AND')],
+      ['with_keywords=', encodeValues(excludedKeywords, 'AND')],
+      ['with_genres=', encodeValues(includedGenres, 'AND')],
+      ['without_genres=', encodeValues(excludedGenres, 'AND')],
+      ['release_date.gte=', startDate],
+      ['release_date.lte=', endDate]
+    ]
+
+    const activeQueryParameters = queryParameters
+      .filter(([_, value]) => value !== '')
+      .map(([param, value]) => param + value)
+
+    const searchQuery = activeQueryParameters.length > 0
+      ? '?' + activeQueryParameters.join('&')
+      : ''
+
+    return client.get(`/discover/${mediaType}${searchQuery}`)
   },
 
   keywords ({ mediaType, id }) {
