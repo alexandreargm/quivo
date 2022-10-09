6b446b8d0a3d92907e9df2cc165ff716d466dbee
alexandreargm <alexandreargm@gmail.com>
2022-10-09 11:35:11 +0200

    build: remove vuex

--- src/main.js
+++ src/main.js
@@ -2,12 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
-import store from './store'
 import axiosInstance from '@/plugins/axios'
 import apiRepositories from './plugins/api-repositories'
 
 const app = createApp(App)
-  .use(store)
   .use(router)
   .use(apiRepositories)
 
