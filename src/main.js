503b3d8cc7c29e54a14e24d1c40481d642877ac1
alexandreargm <alexandreargm@gmail.com>
2022-10-09 12:09:06 +0200

    build(npm): install pinia

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import './registerServiceWorker'
 import router from './router'
 import axiosInstance from '@/plugins/axios'
 import apiRepositories from './plugins/api-repositories'
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
   .use(router)
   .use(apiRepositories)
+  .use(createPinia())
 
 app.config.globalProperties.$http = axiosInstance
 app.mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -16,5 +16,13 @@ module.exports = {
     plugins: [
       // new BundleAnalyzerPlugin()
     ]
+  },
+  chainWebpack: (config) => {
+    config.module
+      .rule('mjs')
+      .test(/\.mjs$/)
+      .type('javascript/auto')
+      .include.add(/node_modules/)
+      .end()
   }
 }
