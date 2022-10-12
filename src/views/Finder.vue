e03dcfada19a579374decad2cbcc07a347a6b0a3
alexandreargm <alexandreargm@gmail.com>
2022-10-12 14:09:01 +0200

    refactor(finder): implement feed gallery and search function

--- src/views/Finder.vue
+++ src/views/Finder.vue
@@ -1,8 +1,16 @@
 <template>
   <finder-layout>
-    <router-view name="search" />
+    <finder-search @submit="searchTitles" />
 
-    <router-view />
+    <base-feed-gallery>
+      <title-card
+        v-for="{id, poster_path} in searchedTitles"
+        :key="id"
+        :id="id"
+        :type="mediaType"
+        :src="'http://image.tmdb.org/t/p/w154/' + poster_path"
+      />
+    </base-feed-gallery>
 
     <template #preview>
       <router-view name="preview" />
@@ -10,7 +18,25 @@
   </finder-layout>
 </template>
 <script setup>
+import { ref } from 'vue'
 import FinderLayout from '../layouts/FinderLayout'
+import FinderSearch from '../components/FinderSearch.vue'
+import repositoryFactory from '@/api/repository-factory.js'
+import { handleRequest } from '../api/request-handlers'
+import BaseFeedGallery from '../components/BaseFeedGallery.vue'
+import TitleCard from '../components/TitleCard.vue'
+const titlesRepository = repositoryFactory.get('titles')
+
+const mediaType = 'movie'
+const searchedTitles = ref([])
+
+const searchTitles = (params) => {
+  handleRequest(titlesRepository.search(params), {
+    onSuccess: ({ data }) => {
+      searchedTitles.value = data.results
+    }
+  })
+}
 </script>
 
 <style lang="scss" scoped>
