eab1210dc08ecdaf09e9c4032109fc64167a8673
alexandreargm <alexandreargm@gmail.com>
2022-10-12 19:43:44 +0200

    feat(components): add size prop

--- src/components/BaseTag.vue
+++ src/components/BaseTag.vue
@@ -1,16 +1,21 @@
 <template>
   <span
     class="base-tag"
-    :class="[props.color, props.variant]"
+    :class="[props.color, props.variant, props.size]"
   >
     <slot>
       {{ props.title }}
     </slot>
+
+    <div class="base-tag__close-button">
+      <base-icon name="CloseIcon" />
+    </div>
   </span>
 </template>
 
 <script setup>
 import { defineProps } from 'vue'
+import BaseIcon from './BaseIcon.vue'
 
 const props = defineProps({
   color: {
@@ -30,23 +35,32 @@ const props = defineProps({
   title: {
     type: String,
     default: ''
+  },
+  size: {
+    type: String,
+    default: 'base',
+    validator: function (value) {
+      return ['', 'sm', 'lg'].includes(value)
+    }
   }
 })
 </script>
 
 <style lang='scss' scoped>
 .base-tag {
-  --bg-color: var(--background-secondary);
   --color: var(--text-neutral);
+  --font-size: var(--font-20);
+  --padding-size: var(--space-20);
+  --height-size: var(--size10);
 
   align-items: center;
-  border-radius: var(--rounded40);
+  border-radius: var(--rounded30);
   color: var(--color);
   display: flex;
-  font-size: var(--font-20);
-  height: var(--size10);
+  font-size: var(--font-size);
+  height: var(--height-size);
   justify-content: center;
-  padding: 0 var(--space-20);
+  padding: 0 var(--padding-size);
   user-select: none;
   width: fit-content;
 }
@@ -70,5 +84,18 @@ const props = defineProps({
 
 .base-tag.secondary {
   border: 1px solid var(--bg-color);
+}
+
+// Size
+.base-tag.sm {
+  --font-size: var(--font-20);
+  --padding-size: var(--space-30);
+  --height-size: var(--size00);
+}
+
+.base-tag.lg {
+  --font-size: var(--font-10);
+  --padding-size: var(--space-10);
+  --height-size: var(--size20);
 }
 </style>
