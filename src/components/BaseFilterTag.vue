010e05ba7c2988cd04aef267cb6d6752dc30ac99
alexandreargm <alexandreargm@gmail.com>
2022-10-12 13:52:29 +0200

    refactor(style): improve color contrast in both color themes

--- src/components/BaseFilterTag.vue
+++ src/components/BaseFilterTag.vue
@@ -107,13 +107,13 @@ if (props.hasExcludes === false) {
 // State
 .base-filter-tag.is-including {
   --bg-color: var(--icon-interactive);
-  --color: var(--text-neutral);
+  --color: var(--white);
   --border-color: var(--text-neutral);
 }
 
 .base-filter-tag.is-excluding {
   --bg-color: var(--icon-danger-secondary);
-  --color: var(--text-neutral);
+  --color: var(--white);
   --border-color: var(--text-neutral);
 
   text-decoration: line-through;
