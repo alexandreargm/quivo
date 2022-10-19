6c0412e62311b5cc7ac221e8da290c5c811d904c
alexandreargm <alexandreargm@gmail.com>
2022-10-19 19:05:44 +0200

    feat(components): create base gallery to display titles in a grid

---
+++ src/components/BaseGallery.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="base-gallery__wrapper">
+    <div class="base-gallery">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import 'container-query-polyfill'
+</script>
+
+<style lang="scss" scoped>
+/* stylelint-disable */
+.base-gallery {
+  display: grid;
+  gap: var(--space-30);
+  grid-template-columns: repeat(3, 1fr);
+
+  @container (min-width: 450px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+
+  @container (min-width: 750px) {
+    grid-template-columns: repeat(5, 1fr);
+  }
+
+  @container (min-width: 1400px) {
+    grid-template-columns: repeat(10, 1fr);
+  }
+
+  &__wrapper {
+    container-type: inline-size;
+  }
+}
+/* stylelint-enable */
+</style>
