be821fd6ce63a68330aa6db990e7aaaeed2142b1
alexandreargm <alexandreargm@gmail.com>
2022-10-26 23:11:33 +0200

    feat(components): finish tag component

--- src/components/BaseTag.vue
+++ src/components/BaseTag.vue
@@ -1,21 +1,33 @@
 <template>
   <span
     class="base-tag"
-    :class="[props.color, props.variant, props.size]"
+    :class="[props.color, props.variant, props.size, getHasCloseClass()]"
   >
-    <slot>
-      {{ props.title }}
-    </slot>
-
-    <div class="base-tag__close-button">
-      <base-icon name="CloseIcon" />
+    <div class="base-tag__title">
+      <slot>
+        {{ props.title }}
+      </slot>
     </div>
+
+    <button
+      v-if="hasClose"
+      class="base-tag__close-button"
+      @click="emits('close', $event)"
+    >
+      <base-icon
+        name="XIcon"
+        size="md"
+        is-fluid
+      />
+    </button>
   </span>
 </template>
 
 <script setup>
-import { defineProps } from 'vue'
+import { defineProps, defineEmits } from 'vue'
 import BaseIcon from './BaseIcon.vue'
+
+const emits = defineEmits(['close'])
 
 const props = defineProps({
   color: {
@@ -38,64 +50,107 @@ const props = defineProps({
   },
   size: {
     type: String,
-    default: 'base',
+    default: '',
     validator: function (value) {
       return ['', 'sm', 'lg'].includes(value)
     }
+  },
+  hasClose: {
+    type: Boolean,
+    default: false
   }
 })
+
+const getHasCloseClass = () => {
+  return props.hasClose ? 'has-close' : ''
+}
 </script>
 
 <style lang='scss' scoped>
 .base-tag {
-  --color: var(--text-neutral);
-  --font-size: var(--font-20);
-  --padding-size: var(--space-20);
-  --height-size: var(--size10);
+  --_bg-color: var(--color);
+  --_color: var(--text);
+  --_font-size: var(--font-20);
+  --_padding-size: var(--space-20);
+  --_height-size: var(--size10);
 
   align-items: center;
   border-radius: var(--rounded30);
-  color: var(--color);
+  color: var(--_color);
   display: flex;
-  font-size: var(--font-size);
-  height: var(--height-size);
+  font-size: var(--_font-size);
+  height: var(--_height-size);
   justify-content: center;
-  padding: 0 var(--padding-size);
+  overflow: hidden;
   user-select: none;
   width: fit-content;
+
+  &__title {
+    padding: 0 var(--_padding-size);
+    white-space: nowrap;
+  }
+
+  &__close-button {
+    appearance: none;
+    background: none;
+    border: 2px solid transparent;
+    font-size: inherit;
+    height: 100%;
+    padding: var(--space-40);
+    width: var(--size10);
+  }
+}
+
+// HasClose
+.base-tag.has-close {
+  .base-tag__title {
+    padding-right: var(--space-30);
+  }
 }
 
 // Color
 .base-tag.interactive {
-  --bg-color: var(--icon-interactive);
-  --color: var(--text-neutral);
+  --_bg-color: var(--icon-interactive);
+  --_color: var(--text-neutral);
 }
 
 .base-tag.danger {
-  --bg-color: var(--icon-danger);
-  --color: var(--text-neutral);
+  --_bg-color: var(--icon-danger);
+  --_color: var(--text-neutral);
 }
 
 // Variant
 .base-tag.primary {
-  background-color: var(--bg-color);
-  border: 1px solid var(--bg-color);
+  background-color: var(--_bg-color);
+  border: 1px solid var(--_bg-color);
+
+  .base-tag__close-button {
+    background: var(--_bg-color);
+  }
 }
 
 .base-tag.secondary {
-  border: 1px solid var(--bg-color);
+  border: 1px solid var(--_bg-color);
 }
 
 // Size
 .base-tag.sm {
-  --font-size: var(--font-20);
-  --padding-size: var(--space-30);
-  --height-size: var(--size00);
+  --_font-size: var(--font-20);
+  --_padding-size: var(--space-30);
+  --_height-size: var(--size00);
+
+  .base-tag__close-button {
+    width: var(--size10);
+  }
 }
 
 .base-tag.lg {
-  --font-size: var(--font-10);
-  --padding-size: var(--space-10);
-  --height-size: var(--size20);
+  --_font-size: var(--font-10);
+  --_padding-size: var(--space-20);
+  --_height-size: var(--size20);
+
+  .base-tag__close-button {
+    width: var(--size20);
+  }
 }
 </style>
