c5c034aec1d320939ecffdf7f0368481ac904cd7
alexandreargm <alexandreargm@gmail.com>
2022-12-10 17:15:21 +0100

    chore: remove unused code

--- src/main.js
+++ src/main.js
@@ -2,14 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
-import axiosInstance from '@/plugins/axios'
-import apiRepositories from './plugins/api-repositories'
 import { createPinia } from 'pinia'
 
 const app = createApp(App)
   .use(router)
-  .use(apiRepositories)
   .use(createPinia())
 
-app.config.globalProperties.$http = axiosInstance
 app.mount('#app')
--- src/plugins/api-repositories.js
+++
@@ -1,7 +0,0 @@
-import repositoryFactory from '../api/repository-factory'
-
-export default {
-  install: (app) => {
-    app.config.globalProperties.$repos = repositoryFactory
-  }
-}
