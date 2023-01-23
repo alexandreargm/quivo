08618a4d1b7629d606986334d28dadd9e7194f0c
alexandreargm <alexandreargm@gmail.com>
2023-01-23 14:48:15 +0100

    feat(components): add has background prop

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,6 +1,7 @@
 <template>
   <div
     class="base-modal"
+    :class="getHasBackgroundClass"
     aria-modal="true"
     role="dialog"
     @click.prevent.self="handleClose"
@@ -27,19 +28,22 @@
   </div>
 </template>
 
-<script>
-export default {
-  emits: ['close'],
+<script setup>
+import { defineProps, defineEmits, computed } from 'vue';
 
-  setup (_, { emit }) {
-    const handleClose = () => {
-      emit('close')
-    }
+const emits = defineEmits(['close'])
 
-    return {
-      handleClose
-    }
+const props = defineProps({
+  hasBackground: {
+    type: Boolean,
+    default: false
   }
+})
+
+const getHasBackgroundClass = computed(() => props.hasBackground ? 'has-background' : '')
+
+const handleClose = () => {
+  emits('close')
 }
 </script>
 
@@ -60,5 +64,10 @@ export default {
     height: fit-content;
     z-index: var(--z-modal);
   }
+}
+
+// hasBackground prop
+.base-modal.has-background {
+  background-color: rgba(0,0,0, 0.2);
 }
 </style>
