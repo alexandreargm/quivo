d17649232ed0e578facb92b0c0db29ab929e70ee
Alexandre Argibay <alexandreargm@gmail.com>
2023-07-29 21:24:39 +0200

    refactor(composables): allow manual enable and disable

--- src/composables/useScrollLock.js
+++ src/composables/useScrollLock.js
@@ -1,9 +1,25 @@
 import { onMounted, onUnmounted } from "vue";
 import { namespacedUuid } from './useUuid'
 
-export function useScrollLock() {
+export function useScrollLock({ immediate = true }) {
   const uniqueElementId = namespacedUuid('scroll-lock')
 
-  onMounted(() => document.body.classList.add(uniqueElementId))
-  onUnmounted(() => document.body.classList.remove(uniqueElementId))
+  function enable() {
+    document.body.classList.add(uniqueElementId)
+  }
+
+  function disable() {
+    document.body.classList.remove(uniqueElementId)
+  }
+
+  if (immediate) {
+    onMounted(enable)
+  }
+
+  onUnmounted(disable)
+
+  return {
+    enable,
+    disable
+  }
 }
