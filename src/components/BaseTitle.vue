c443b8bb9b2f019d6b9d9bf1301ef47ea17c42a9
Alexandre Argibay <alexandreargm@gmail.com>
2023-08-13 17:32:22 +0200

    refactor(components): add font size variations and allow disabling margin bottom

--- src/components/BaseTitle.vue
+++ src/components/BaseTitle.vue
@@ -2,7 +2,10 @@
   <component
     :is="getLevelTag()"
     class="base-title"
-    :class="getLevelClass()"
+    :class="{
+      ['is-level-' + props.level]: true,
+      'has-margin-bottom': props.hasMarginBottom
+    }"
   >
     <slot>{{ props.title }}</slot>
   </component>
@@ -12,24 +15,25 @@
 import { defineProps } from 'vue'
 const props = defineProps({
   level: {
-    type: String,
+    type: Number,
     required: true,
-    default: '1',
+    default: 1,
     validator: function (value) {
-      return ['1', '2', '3', '4', '5', '6'].includes(value)
+      return [1, 2, 3, 4, 5, 6].includes(value)
     }
   },
   title: {
     type: String,
     default: ''
+  },
+  hasMarginBottom: {
+    type: Boolean,
+    default: true
   }
 })
 
 const getLevelTag = () => {
   return 'h' + props.level
-}
-const getLevelClass = () => {
-  return 'is-level-' + props.level
 }
 </script>
 
@@ -40,36 +44,39 @@ const getLevelClass = () => {
   font-weight: var(--medium);
   line-height: var(--line-20);
   white-space: nowrap;
-  margin-bottom: clamp(var(--space-10), 0.5em, 3rem );
+  
+  &.has-margin-bottom {
+    margin-bottom: clamp(var(--space-10), 0.5em, 3rem );
+  }
 
   &.is-level-1 {
+    --min-size: var(--font40);
+    --max-size: var(--font60);
+  }
+
+  &.is-level-2 {
     --min-size: var(--font30);
     --max-size: var(--font40);
   }
 
-  &.is-level-2 {
-    --min-size: var(--font10);
-    --max-size: var(--font30);
-  }
-
   &.is-level-3 {
-    --min-size: var(--font10);
+    --min-size: var(--font20);
     --max-size: var(--font30);
   }
 
   &.is-level-4 {
     --min-size: var(--font10);
-    --max-size: var(--font30);
+    --max-size: var(--font20);
   }
 
   &.is-level-5 {
-    --min-size: var(--font10);
-    --max-size: var(--font30);
+    --min-size: var(--font-10);
+    --max-size: var(--font00);
   }
 
   &.is-level-6 {
-    --min-size: var(--font10);
-    --max-size: var(--font30);
+    --min-size: var(--font-20);
+    --max-size: var(--font-10);
   }
 }
 </style>
