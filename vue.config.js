b6a7cb77b231225d337e09adf8e66e7dd8f9bc49
Alexandre Argibay <alexandreargm@gmail.com>
2023-08-13 19:37:06 +0200

    refactor(build): implement svg loader

--- vue.config.js
+++ vue.config.js
@@ -28,5 +28,16 @@ module.exports = {
       .type('javascript/auto')
       .include.add(/node_modules/)
       .end()
+
+      config.module.rules.delete('svg')
+
+      config.module
+        .rule('svg')
+        .test(/\.(svg)(\?.*)?$/)
+        .use('vue-loader')
+        .loader('vue-loader')
+        .end()
+        .use('vue-svg-loader')
+        .loader('vue-svg-loader')
   }
 }
