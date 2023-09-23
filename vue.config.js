0e8347be31ec11ed99dfa0e5ae0c64af33854e00
Alexandre Argibay <alexandreargm@gmail.com>
2023-09-23 18:41:55 +0200

    build(vue): use default public path

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
-  // Set route for gh-pages. Ref.: https://cli.vuejs.org/config/#publicpath
-  publicPath: process.env.NODE_ENV === 'production' ? '/quivo/' : '/',
   css: {
     // ref: https://cli.vuejs.org/guide/css.html#passing-options-to-pre-processor-loaders
     loaderOptions: {
