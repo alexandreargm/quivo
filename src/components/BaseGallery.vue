1ce143ec55d8a8701c5d48dde9d6c534c25235a5
Alexandre Argibay <alexandreargm@gmail.com>
2023-11-04 17:49:27 +0100

    refactor(components): add column count prop controls

--- src/components/BaseGallery.vue
+++ src/components/BaseGallery.vue
@@ -7,31 +7,53 @@
 </template>
 
 <script setup>
-import 'container-query-polyfill'
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  mobileColumnCount: {
+    type: Number,
+    default: 2
+  },
+  mobileLgColumnCount: {
+    type: Number,
+    default: 4
+  },
+  tabletColumnCount: {
+    type: Number,
+    default: 5
+  },
+  desktopColumnCount: {
+    type: Number,
+    default: 10
+  }
+})
 </script>
 
 <style lang="scss" scoped>
-/* stylelint-disable */
 .base-gallery {
+  --_mobile-column-count: v-bind(props.mobileColumnCount);
+  --_mobile-lg-column-count: v-bind(props.mobileLgColumnCount);
+  --_tablet-column-count: v-bind(props.tabletColumnCount);
+  --_desktop-column-count: v-bind(props.desktopColumnCount);
+
   display: grid;
-  gap: var(--space-30);
-  grid-template-columns: repeat(2, 1fr);
+  gap: var(--space-20);
+  grid-template-columns: repeat(var(--_mobile-column-count), 1fr);
 
   @container (min-width: 450px) {
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(var(--_mobile-column-count), 1fr);
   }
 
   @container (min-width: 750px) {
-    grid-template-columns: repeat(5, 1fr);
+    grid-template-columns: repeat(var(--_tablet-column-count), 1fr);
   }
 
   @container (min-width: 1400px) {
-    grid-template-columns: repeat(10, 1fr);
+    grid-template-columns: repeat(var(--_desktop-column-count), 1fr);
   }
 
   &__wrapper {
     container-type: inline-size;
   }
 }
-/* stylelint-enable */
 </style>
